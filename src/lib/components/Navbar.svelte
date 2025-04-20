4979355409d21506e43e8ed2a605b0bc76d6e965
Lucas <lucaswade4@gmail.com>
2025-04-20 13:42:22 -0700

    Add navbar and initial styling

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,17 @@
+<header class="bg-transparent">
+	<div class="flex w-screen items-center justify-between p-3">
+		<div class="text-teal-600">
+			<span class="italic">jules<span class="font-bold text-teal-500">Cooks</span></span>
+		</div>
+		<div class="flex w-1/3 flex-col">
+			<ul class="flex justify-between">
+				<li><a href="#">About</a></li>
+				<li><a href="#">Classes</a></li>
+				<li><a href="#">Products</a></li>
+			</ul>
+		</div>
+		<div class="">
+			<a href="#" class="rounded-lg bg-teal-600 p-3 text-white">Book now</a>
+		</div>
+	</div>
+</header>
